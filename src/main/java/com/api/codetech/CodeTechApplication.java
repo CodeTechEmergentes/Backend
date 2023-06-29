df445fd5601ca6b589269ca8bd78384488e494f2
Sebastian Santiago Alfaro Mendoza <sebas43243@hotmail.com>
2023-06-29 08:38:24 -0500

    added annotations

--- src/main/java/com/api/codetech/CodeTechApplication.java
+++ src/main/java/com/api/codetech/CodeTechApplication.java
@@ -1,11 +1,13 @@
 package com.api.codetech;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@OpenAPIDefinition
 public class CodeTechApplication {
 
     public static void main(String[] args) {
