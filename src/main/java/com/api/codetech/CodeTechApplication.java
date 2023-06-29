43091d9f0fc5b8b0a29e14eb507e7786e5a5ee73
Sebastian Santiago Alfaro Mendoza <sebas43243@hotmail.com>
2023-06-29 05:19:57 -0500

    changed swagger

--- src/main/java/com/api/codetech/CodeTechApplication.java
+++ src/main/java/com/api/codetech/CodeTechApplication.java
@@ -1,24 +1,19 @@
 package com.api.codetech;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@OpenAPIDefinition
 @SpringBootApplication
-@EnableJpaAuditing
 public class CodeTechApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CodeTechApplication.class, args);
     }
 
-    @Bean
+    /*@Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
@@ -29,5 +24,5 @@ public class CodeTechApplication {
                         .allowedHeaders("*");
             }
         };
-    }
+    }*/
 }
