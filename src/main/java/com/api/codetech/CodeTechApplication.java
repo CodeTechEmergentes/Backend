2a2b9a1a2a51704868751c41bf7780d25b2ef8f6
Sebastian Santiago Alfaro Mendoza <sebas43243@hotmail.com>
2023-06-29 05:02:04 -0500

    updated sweagger

--- src/main/java/com/api/codetech/CodeTechApplication.java
+++ src/main/java/com/api/codetech/CodeTechApplication.java
@@ -6,13 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @OpenAPIDefinition
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableWebMvc
 public class CodeTechApplication {
 
     public static void main(String[] args) {
@@ -21,13 +20,10 @@ public class CodeTechApplication {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
+        return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*");
+                registry.addMapping("/**").allowedOrigins("*");
             }
         };
     }
