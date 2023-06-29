d0e310dd6a6bac308d82e87ea779267d7ed1e31c
Sebastian Santiago Alfaro Mendoza <sebas43243@hotmail.com>
2023-06-29 04:42:27 -0500

    updated cors config

--- src/main/java/com/api/codetech/CodeTechApplication.java
+++ src/main/java/com/api/codetech/CodeTechApplication.java
@@ -3,15 +3,32 @@ package com.api.codetech;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @OpenAPIDefinition
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableWebMvc
 public class CodeTechApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CodeTechApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
